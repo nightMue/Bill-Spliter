bcda2c70f6fe32d232bc33adbd3da564c61e6a35
Nicholas Mueller <34995238+nightMue@users.noreply.github.com>
2019-08-14 18:54:56 -0500

    Added firendView file for adjustable split ratio

---
+++ lib/ui/slider/friendView.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FriendView extends StatefulWidget {
+  @override
+  _FriendViewState createState() => _FriendViewState();
+}
+
+class _FriendViewState extends State<FriendView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
