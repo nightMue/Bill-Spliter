cad59b60e720445475f89bc7d6145f074d1be9cd
Nicholas Mueller <34995238+nightMue@users.noreply.github.com>
2019-09-05 19:10:59 -0500

    Added inkresponse to calculator buttons

--- lib/ui/calculator.dart
+++ lib/ui/calculator.dart
@@ -71,69 +71,203 @@ class _CalculatorState extends State<Calculator> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-                GestureDetector(
-                  child: CustomNumberButton("1", false),
-                  onTap: () => pressed(1),
+                //GestureDetector(
+                //  child: CustomNumberButton("1", false),
+                //  onTap: () => pressed(1),
+                //),
+                Material(
+                  color: Colors.transparent,
+                  child:InkResponse(
+                    radius: 30,
+                    highlightShape: BoxShape.circle,
+                    highlightColor: Colors.transparent,
+                    splashColor: Color.fromRGBO(57, 153, 66, 0.5),
+                    child: CustomNumberButton("1",  false),
+                    onTap: () => pressed(1),
+                  ),
                 ),
-                GestureDetector(
-                  child: CustomNumberButton("2", false),
-                  onTap: () => pressed(2),
+                //GestureDetector(
+                //  child: CustomNumberButton("2", false),
+                //  onTap: () => pressed(2),
+                //),
+                Material(
+                  color: Colors.transparent,
+                  child:InkResponse(
+                    radius: 30,
+                    highlightShape: BoxShape.circle,
+                    highlightColor: Colors.transparent,
+                    splashColor: Color.fromRGBO(57, 153, 66, 0.5),
+                    child: CustomNumberButton("2",  false),
+                    onTap: () => pressed(2),
+                  ),
                 ),
-                GestureDetector(
-                  child: CustomNumberButton("3", false),
-                  onTap: () => pressed(3),
+                //GestureDetector(
+                //  child: CustomNumberButton("3", false),
+                //  onTap: () => pressed(3),
+                //),
+                Material(
+                  color: Colors.transparent,
+                  child:InkResponse(
+                    radius: 30,
+                    highlightShape: BoxShape.circle,
+                    highlightColor: Colors.transparent,
+                    splashColor: Color.fromRGBO(57, 153, 66, 0.5),
+                    child: CustomNumberButton("3",  false),
+                    onTap: () => pressed(3),
+                  ),
                 ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-                GestureDetector(
-                  child: CustomNumberButton("4", false),
-                  onTap: () => pressed(4),
+                //GestureDetector(
+                //  child: CustomNumberButton("4", false),
+                //  onTap: () => pressed(4),
+                //),
+                Material(
+                  color: Colors.transparent,
+                  child:InkResponse(
+                    radius: 30,
+                    highlightShape: BoxShape.circle,
+                    highlightColor: Colors.transparent,
+                    splashColor: Color.fromRGBO(57, 153, 66, 0.5),
+                    child: CustomNumberButton("4",  false),
+                    onTap: () => pressed(4),
+                  ),
                 ),
-                GestureDetector(
-                  child: CustomNumberButton("5", false),
-                  onTap: () => pressed(5),
+                //GestureDetector(
+                //  child: CustomNumberButton("5", false),
+                //  onTap: () => pressed(5),
+                //),
+                Material(
+                  color: Colors.transparent,
+                  child:InkResponse(
+                    radius: 30,
+                    highlightShape: BoxShape.circle,
+                    highlightColor: Colors.transparent,
+                    splashColor: Color.fromRGBO(57, 153, 66, 0.5),
+                    child: CustomNumberButton("5",  false),
+                    onTap: () => pressed(5),
+                  ),
                 ),
-                GestureDetector(
-                  child: CustomNumberButton("6", false),
-                  onTap: () => pressed(6),
+                //GestureDetector(
+                //  child: CustomNumberButton("6", false),
+                //  onTap: () => pressed(6),
+                //),
+                Material(
+                  color: Colors.transparent,
+                  child:InkResponse(
+                    radius: 30,
+                    highlightShape: BoxShape.circle,
+                    highlightColor: Colors.transparent,
+                    splashColor: Color.fromRGBO(57, 153, 66, 0.5),
+                    child: CustomNumberButton("6",  false),
+                    onTap: () => pressed(6),
+                  ),
                 ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-                GestureDetector(
-                  child: CustomNumberButton("7", false),
-                  onTap: () => pressed(7),
+                //GestureDetector(
+                //  child: CustomNumberButton("7", false),
+                //  onTap: () => pressed(7),
+                //),
+                Material(
+                  color: Colors.transparent,
+                  child:InkResponse(
+                    radius: 30,
+                    highlightShape: BoxShape.circle,
+                    highlightColor: Colors.transparent,
+                    splashColor: Color.fromRGBO(57, 153, 66, 0.5),
+                    child: CustomNumberButton("7",  false),
+                    onTap: () => pressed(7),
+                  ),
                 ),
-                GestureDetector(
-                  child: CustomNumberButton("8", false),
-                  onTap: () => pressed(8),
+                //GestureDetector(
+                //  child: CustomNumberButton("8", false),
+                //  onTap: () => pressed(8),
+                //),
+                Material(
+                  color: Colors.transparent,
+                  child:InkResponse(
+                    radius: 30,
+                    highlightShape: BoxShape.circle,
+                    highlightColor: Colors.transparent,
+                    splashColor: Color.fromRGBO(57, 153, 66, 0.5),
+                    child: CustomNumberButton("8",  false),
+                    onTap: () => pressed(8),
+                  ),
                 ),
-                GestureDetector(
-                  child: CustomNumberButton("9", false),
-                  onTap: () => pressed(9),
+                //GestureDetector(
+                //  child: CustomNumberButton("9", false),
+                //  onTap: () => pressed(9),
+                //),
+                Material(
+                  color: Colors.transparent,
+                  child:InkResponse(
+                    radius: 30,
+                    highlightShape: BoxShape.circle,
+                    highlightColor: Colors.transparent,
+                    splashColor: Color.fromRGBO(57, 153, 66, 0.5),
+                    child: CustomNumberButton("9",  false),
+                    onTap: () => pressed(9),
+                  ),
                 ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-                GestureDetector(
-                  child: CustomNumberButton(".", false),
-                  onTap: () => _pressedDecimal(),
+                Material(
+                  color: Colors.transparent,
+                  child:InkResponse(
+                    radius: 30,
+                    highlightShape: BoxShape.circle,
+                    highlightColor: Colors.transparent,
+                    splashColor: Color.fromRGBO(57, 153, 66, 0.5),
+                    child: CustomNumberButton(".",  false),
+                    onTap: () => _pressedDecimal(),
+                  ),
                 ),
-                GestureDetector(
-                  child: CustomNumberButton("0", false),
-                  onTap: () => pressed(0),
+                //GestureDetector(
+                //  child: CustomNumberButton(".", false),
+                //  onTap: () => _pressedDecimal(),
+                //),
+                Material(
+                  color: Colors.transparent,
+                  child:InkResponse(
+                    radius: 30,
+                    highlightShape: BoxShape.circle,
+                    highlightColor: Colors.transparent,
+                    splashColor: Color.fromRGBO(57, 153, 66, 0.5),
+                    child: CustomNumberButton("0",  false),
+                    onTap: () => pressed(0),
+                  ),
                 ),
-                GestureDetector(
-                  child: CustomNumberButton("->", true),
-                  onTap: () => _pressedBack(),
+                //GestureDetector(
+                //  child: CustomNumberButton("0", false),
+                //  onTap: () => pressed(0),
+                //),
+                Material(
+                  color: Colors.transparent,
+                  child:InkResponse(
+                    radius: 30,
+                    highlightShape: BoxShape.circle,
+                    highlightColor: Colors.transparent,
+                    splashColor: Color.fromRGBO(57, 153, 66, 0.5),
+                    child: CustomNumberButton("->",  true),
+                    onTap: () => print("BACK ONE"),
+                    onLongPress: () => _pressedBack(),
+                  ),
                 ),
+                //GestureDetector(
+                //  child: CustomNumberButton("->", true),
+                //  onLongPress: () => _pressedBack(),
+                //  onTap: () => print("BACK ONE"),
+                //),
               ],
             ),
             
